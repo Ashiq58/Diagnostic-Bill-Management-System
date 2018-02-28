5f198b4969445fc3c6cb94ff6e4bcd772bbc2e67
Ashiq58 <ashikul58bd@gmail.com>
2018-02-28 09:54:35 +0600

    Diagnostic-Bill-Management-System

---
+++ Diagnostic-Bill-Management-System/DignosticCenterBillManagementSystemVS2013/BLL/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DignosticCenterBillManagementSystemVS2013.BLL
+{
+    public class Class1
+    {
+    }
+}
